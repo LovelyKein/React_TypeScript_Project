340cd5b952d42532cfdc4fb4f39d0311c339d227
LovelyKein <2312197273@qq.com>
2023-02-15 01:19:55 +0800

    add router files, router rules

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,25 +1,152 @@
 import { lazy } from "react";
-import { RouteObject, redirect, Navigate } from "react-router-dom";
+import { RouteObject, Navigate } from "react-router-dom";
 
 const Login = lazy(() => import('@/views/login/Login'))
-const Home = lazy(() => import("@/views/home/Home"));
+const Layout = lazy(() => import("@/components/layout/Layout"))
+
+const Home = lazy(() => import("@/views/admin/home/Home"))
+
+const AllList = lazy(() => import("@/views/admin/user/user_list/UserList"))
+
+const Role = lazy(() => import("@/views/admin/authority/role/Role"))
+const Permission = lazy(() => import("@/views/admin/authority/permission/Permission"))
+
+const Edit = lazy(() => import("@/views/admin/news/edit/Edit"))
+const Draft = lazy(() => import("@/views/admin/news/draft/Draft"))
+const Classify = lazy(() => import("@/views/admin/news/classify/Classify"))
+
+const CheckNews = lazy(() => import("@/views/admin/check/check_news/CheckNews"))
+const CheckList = lazy(() => import("@/views/admin/check/check_list/CheckList"))
+
+const PublishAwait = lazy(() => import("@/views/admin/publish/await/Await"))
+const PublishDone = lazy(() => import("@/views/admin/publish/done/Done"))
+
+const Skeleton = lazy(() => import("@/views/helper/skeleton/Skeleton"))
 
 const router: RouteObject[] = [
   {
     path: "/",
     index: true,
     element: <Navigate to={"/login"} />, // <Navigate /> 组件被渲染时，就会跳转到 to 的路径页面
-    // loader: () => {
-    //   redirect('/login')
-    // }
   },
   {
     path: "/login",
     element: <Login />,
   },
   {
-    path: "/home",
-    element: <Home />,
+    path: "/admin",
+    element: <Layout />,
+    children: [
+      // 首页
+      {
+        path: "/admin",
+        element: <Navigate to={"/admin/home"} />, // 重定向
+      },
+      {
+        path: "home",
+        element: <Home />,
+        index: true,
+      },
+      // 用户
+      {
+        path: "user",
+        children: [
+          {
+            path: "/admin/user",
+            element: <Navigate to={"/admin/user/all_list"} />,
+          },
+          {
+            path: "all_list",
+            element: <AllList />,
+            index: true,
+          },
+        ],
+      },
+      // 权限
+      {
+        path: "authority",
+        children: [
+          {
+            path: "/admin/authority",
+            element: <Navigate to={"/admin/authority/role"} />,
+          },
+          {
+            path: "role",
+            element: <Role />,
+            index: true,
+          },
+          {
+            path: "permission",
+            element: <Permission />,
+          },
+        ],
+      },
+      // 新闻
+      {
+        path: "news",
+        children: [
+          {
+            path: "/admin/news",
+            element: <Navigate to={"/admin/news/edit"} />,
+          },
+          {
+            path: "edit",
+            element: <Edit />,
+            index: true,
+          },
+          {
+            path: "draft",
+            element: <Draft />,
+          },
+          {
+            path: "classify",
+            element: <Classify />,
+          },
+        ],
+      },
+      // 审核
+      {
+        path: "check",
+        children: [
+          {
+            path: "/admin/check",
+            element: <Navigate to={"/admin/check/news"} />,
+          },
+          {
+            path: "news",
+            element: <CheckNews />,
+            index: true,
+          },
+          {
+            path: "list",
+            element: <CheckList />,
+          },
+        ],
+      },
+      // 发布
+      {
+        path: "publish",
+        children: [
+          {
+            path: "/admin/publish",
+            element: <Navigate to={"/admin/publish/await"} />,
+          },
+          {
+            path: "await",
+            element: <PublishAwait />,
+            index: true,
+          },
+          {
+            path: "done",
+            element: <PublishDone />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "*", // * 以上路径都不匹配的情况
+    element: <Skeleton />,
   },
 ];
 
--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -3,12 +3,14 @@ import './Login.scss'
 
 import router from '@/router'
 
-import { useLocation, matchRoutes } from 'react-router-dom'
+import { useLocation, matchRoutes, useSearchParams } from 'react-router-dom'
 
 export default function Login() {
   const location = useLocation()
   const routes = matchRoutes(router, location.pathname);
-  console.log(routes)
+  const [ searchParams, setSearchParams ] = useSearchParams()
+  // console.log(searchParams.get('name'))
+  // console.log(routes)
   // console.log(location)
   return (
     <div className='login'>Login</div>
